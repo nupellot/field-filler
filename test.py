fbb986347f19d51650010882382a0855aa8edd46
Dmitriy Korotkov <nupellot@gmail.com>
2023-10-03 02:37:09 +0300

    Добавил обработку ещё нескольких исключений

--- test.py
+++ test.py
@@ -1,55 +1,60 @@
-import sys
-import time
-
 import pandas as pandas
 import requests as requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import openpyxl
 
+TABLE_NAME = "challenge.xlsx"  # Имя, под которым будет сохраняться таблица с данными.
+TABLE_DIRECTORY = ""
+SCREENSHOT_DIRECTORY = ""
+DEFAULT_DIRECTORY = ""
 
-# Загрузка веб-страницы
+# Подготовительная работа. #
+
+# Загрузка веб-страницы.
 url = "https://www.rpachallenge.com/"  # URL страницы, с которой мы будем работать.
-driver = webdriver.Chrome()  # Используем Selenium WebDriver (например, Chrome, Firefox, Safari)
+driver = webdriver.Chrome()  # Используем Selenium WebDriver (например, Chrome, Firefox, Safari).
 driver.get(url)
 
-
 try:
-	print("Скачиваем таблицу с исходными данными...")
-	response = requests.get("https://www.rpachallenge.com/assets/downloadFiles/challenge.xlsx")
-	print("\033[42mСкачивание успешно завершено\033[0m")
-	if len(sys.argv) >= 2:
-		file = open(str(sys.argv[1]) + "challenge.xlsx", "wb")
-		file.write(response.content)
+    # Забираем таблицу.
+    print("Скачиваем таблицу с исходными данными...")
+    response = requests.get("https://www.rpachallenge.com/assets/downloadFiles/challenge.xlsx")
+    print("\033[42mСкачивание успешно завершено\033[0m")
+
+    # Обработка необязательных параметров.
+    if TABLE_DIRECTORY:  # Если указан адрес сохранения таблицы.
+        TABLE_PATH = TABLE_DIRECTORY + TABLE_NAME
+    else:  # Если не указан адрес сохранения таблицы, используем стандартный.
+        TABLE_PATH = TABLE_DIRECTORY + TABLE_NAME
+    file = open(TABLE_PATH, "wb")
+    file.write(response.content)  # Записываем данные в файл.
+    file.close()
+    sheet = pandas.read_excel(TABLE_PATH)  # Читаем все данные из таблицы.
+
 except requests.exceptions.RequestException:
-	print("Ошибка при получении файла с сервера: ", Exception)
-except Exception:
-	print(Exception)
-
-
-sheet = pandas.read_excel('challenge.xlsx')
+    print("Ошибка при получении файла с сервера: ", Exception)
+    print("Используем заготовленную заранее копию.")
+    sheet = pandas.read_excel(TABLE_NAME)
 
 # Нажимаем на кнопку "Старт".
 xpath_expression = f"//button[text() = 'Start']"
 start_element = driver.find_element(By.XPATH, xpath_expression)
 start_element.click()
 
-for row in sheet.iloc():
-	for field_name, field_value in row.items():
-		# print("key, value: ", key, value)
-		# Определяем label, удаляя начальные и конечные пробелы.
-		label_text = field_name.strip()
-		# Используем XPath для нахождения input по тексту label
-		xpath_expression = f"//label[text()='{label_text}']/following-sibling::input"
-		input_element = driver.find_element(By.XPATH, xpath_expression)
+for row in sheet.iloc():  # Итерируемся по строкам.
+    for field_name, field_value in row.items():  # Итерируемся по всем объектам в строке.
+        # Определяем label, удаляя начальные и конечные пробелы.
+        label_text = field_name.strip()
+        # Используем XPath для нахождения input по тексту соответствующего label.
+        xpath_expression = f"//label[text()='{label_text}']/following-sibling::input"
+        # Находим поле, куда будем вводить данные.
+        input_element = driver.find_element(By.XPATH, xpath_expression)
+        # Заполняем определенное поле формы.
+        input_element.send_keys(str(field_value))
 
-		input_element.send_keys(str(field_value))
-
-	# time.sleep(0.2)
-	# Отправить форму
-	submit_button = driver.find_element(By.CSS_SELECTOR, "input[type='submit']")
-	submit_button.click()
-
+    # Отправляем форму
+    submit_button = driver.find_element(By.CSS_SELECTOR, "input[type='submit']")
+    submit_button.click()
 
 # Условие для контроля закрытия браузера
 close_browser = input("Нажмите Enter, чтобы закрыть браузер...")
