d963d925a664ddde5706f7a0174d16282e2be39a
Dmitriy Korotkov <nupellot@gmail.com>
2023-10-03 01:42:41 +0300

    Переписал работу с БД на pandas

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import sys
+import time
 
+import pandas as pandas
 import requests as requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -25,27 +27,16 @@ except Exception:
 	print(Exception)
 
 
-# Открываем файл Excel
-workbook = openpyxl.load_workbook('challenge.xlsx')
-sheet = workbook.active
+sheet = pandas.read_excel('challenge.xlsx')
 
-
+# Нажимаем на кнопку "Старт".
 xpath_expression = f"//button[text() = 'Start']"
 start_element = driver.find_element(By.XPATH, xpath_expression)
 start_element.click()
 
-first_row_processed = False
-
-for row in sheet.iter_rows(values_only=True):
-	if not first_row_processed:
-		first_row_processed = True
-		continue
-
-	# Помимо данных текущей строки всегда забираем с собой заголовки таблицы.
-	form_data = dict(zip([i.value for i in sheet[1]], row))
-
-	# Вводим все данные в нужные поля формы.
-	for field_name, field_value in form_data.items():
+for row in sheet.iloc():
+	for field_name, field_value in row.items():
+		# print("key, value: ", key, value)
 		# Определяем label, удаляя начальные и конечные пробелы.
 		label_text = field_name.strip()
 		# Используем XPath для нахождения input по тексту label
@@ -54,13 +45,11 @@ for row in sheet.iter_rows(values_only=True):
 
 		input_element.send_keys(str(field_value))
 
-	# time.sleep(0.1)
+	# time.sleep(0.2)
 	# Отправить форму
 	submit_button = driver.find_element(By.CSS_SELECTOR, "input[type='submit']")
 	submit_button.click()
 
-
-# submit_button.click()
 
 # Условие для контроля закрытия браузера
 close_browser = input("Нажмите Enter, чтобы закрыть браузер...")
